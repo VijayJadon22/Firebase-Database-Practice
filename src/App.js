7ca2183368f146c60cb758486038dc3e18165367
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-01-19 00:32:30 +0530

    Implement all the functions of the fireBase database

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import { getFirestore, collection, addDoc, doc, getDoc, query, where, getDocs, updateDoc, deleteDoc, deleteField } from "firebase/firestore";
+import { app } from "./firebase"
 import './App.css';
 
+// Initialize Cloud Firestore and get a reference to the service
+const firestore = getFirestore(app);
+
 function App() {
+
+  const writeData = async () => {
+    const collectionRef = collection(firestore, "cities");
+    const result = await addDoc(collectionRef, {
+      name: "Delhi",
+      pinCode: 1234,
+      lat: 123,
+      long: 456
+    });
+  };
+
+  const makeSubCollection = async () => {
+    const result = await addDoc(collection(firestore, "cities/vDMVcSJvs2AJgRa4SoLs/places"), {
+      name: "Rajeev Chowk",
+      desc: "Very crowded"
+    });
+
+  };
+
+  const getDocument = async () => {
+    const docRef = doc(firestore, "cities", "vDMVcSJvs2AJgRa4SoLs");
+    const snap = await getDoc(docRef);
+    console.log(snap.data());
+  };
+
+  const getDocumentsByQuery = async () => {
+    const usersRef = collection(firestore, "users");
+    const q = query(usersRef, where("isMale", "==", true));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => console.log(doc.data()));
+  };
+
+  const updateDocument = async () => {
+    const docRef = doc(firestore, "cities", "vDMVcSJvs2AJgRa4SoLs");
+    await updateDoc(docRef, {
+      name: "New Delhi"
+    });
+  };
+
+  const deleteDocument = async () => {
+    await deleteDoc(doc(firestore, "users", "sZ9C0VxJiFh46kTVMnmO"));
+  };
+
+  const deleteFieldFromDocument = async () => {
+    const docRef = doc(firestore, "users", "N2laGkaa8VWeAiKGOLyu");
+    await updateDoc(docRef, {
+      Tall: deleteField()
+    })
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Firebase Firestore</h1>
+      <button onClick={writeData}>Put Data</button>
+      <button onClick={makeSubCollection}>Put SubData</button>
+      <button onClick={getDocument}>Get Doc</button>
+      <button onClick={getDocumentsByQuery}>Get Docs on query</button>
+      <button onClick={updateDocument}>Update document</button>
+      <button onClick={deleteDocument}>Delete document</button>
+      <button onClick={deleteFieldFromDocument}>Delete Field</button>
     </div>
   );
 }
